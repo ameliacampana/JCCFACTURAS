9e9952758fdcba7b2a7787b68445581f6e2a3b45
ameliacampana <amelia.campana@gmail.com>
2026-07-15 08:30:59 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'jccfacturas-v1';
-const ASSETS = ['./','./index.html','./manifest.json'];
+const CACHE = 'jccfacturas-v2';
+const ASSETS = ['./','./index.html','./manifest.json','./msal-browser.min.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
